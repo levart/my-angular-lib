737bc6379c2e158c1efeed2f222459b26d569e33
levan jmukhadze <jmukhadze@gmail.com>
2025-09-03 20:50:17 +0400

    Initial library commit

---
+++ projects/my-shared-lib/src/lib/my-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my',
+  template: `<p>Hello from my shared library!</p>`,
+  styles: []
+})
+export class MyComponent { }
---
+++ projects/my-shared-lib/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of my-shared-lib
+ */
+
+export * from './lib/my-component';
